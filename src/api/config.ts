d338937ae47322bda8d8b72fcb8c29432a4dea74
Max Parker <maxrparker@gmail.com>
2022-08-18 09:56:08 -0700

    DB config changes

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,25 +1,37 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 let path;
 switch (process.env.NODE_ENV) {
-  case "test":
-    path = `.env.test`;
-    break;
-  case "production":
-    path = `.env.production`;
-    break;
-  default:
-    path = `.env.development`;
+	case 'test':
+		path = `.env.test`;
+		break;
+	case 'production':
+		path = `.env.production`;
+		break;
+	default:
+		path = `.env.development`;
 }
 dotenv.config({ path: path });
 
-export const API_PORT = parseInt(process.env.API_PORT || "3000");
-export const FRONTEND_URL = process.env.FRONTEND_URL || "";
-export const AUTH_REDIRECT = process.env.AUTH_REDIRECT || process.env.FRONTEND_URL || "";
+export const API_PORT = parseInt(process.env.API_PORT || '3000');
+export const FRONTEND_URL = process.env.FRONTEND_URL || '';
+export const AUTH_REDIRECT =
+	process.env.AUTH_REDIRECT || process.env.FRONTEND_URL || '';
 export const NODE_ENV = process.env.NODE_ENV;
 
-export const DB_NAME = process.env.DB_NAME || "postgres";
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_PASS = process.env.DB_PASS || "password";
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_PORT = process.env.DB_PORT || "5432";
+export const DB_NAME = process.env.DB_NAME || 'postgres';
+export const DB_USER = process.env.DB_USER || 'postgres';
+export const DB_PASS = process.env.DB_PASS || 'password';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || '5432';
+
+export const DB_CONFIG = {
+	client: 'mssql',
+	connection: {
+		host: DB_HOST,
+		user: DB_USER,
+		password: DB_PASS,
+		database: DB_NAME,
+		port: parseInt(DB_PORT),
+	},
+};

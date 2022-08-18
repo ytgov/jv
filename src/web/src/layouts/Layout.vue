36a02e93fb6cfa82ab31d952de906f5141e4afca
Michael Johnson <michael@icefoganalytics.com>
2022-08-18 10:42:23 -0700

    FE updates

--- src/web/src/layouts/Layout.vue
+++ src/web/src/layouts/Layout.vue
@@ -34,7 +34,8 @@
       height="70"
       style="left: 0; border-bottom: 3px #f3b228 solid"
     >
-      <img src="/yukon.svg" style="margin: -8px 85px 0 0" height="44" />
+      <!-- <v-icon color="#f3b228" class="mr-5">{{ applicationIcon }}</v-icon> -->
+      <img src="/yukon.svg" style="margin: -8px 155px 0 0" height="44" />
       <v-toolbar-title>
         <span style="font-weight: 700">{{ applicationName }}</span>
 
@@ -48,14 +49,19 @@
         ></v-progress-circular>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <!-- <v-label dark>License Year:</v-label>
+      <v-select
+        v-model="licenseYear"
+        smaller
+        :items="licenseYears"
+        dense
+        style="margin-left: 15px; max-width: 150px; margin-right: 20px"
+        hide-details
+      ></v-select> -->
 
-      <div>
-        <v-btn color="primary" text class="mr-1" to="/dashboard"
-          ><v-icon>mdi-home</v-icon></v-btn
-        >
-        <v-divider class="mr-5" vertical inset></v-divider>
+      <div v-if="isAuthenticated">
         <span>{{ username }}</span>
-        <v-menu offset-y class="ml-0">
+        <v-menu bottom left class="ml-0">
           <template v-slot:activator="{ on, attrs }">
             <v-btn icon color="primary" v-bind="attrs" v-on="on">
               <v-icon>mdi-dots-vertical</v-icon>
@@ -69,14 +75,8 @@
               </v-list-item-icon>
               <v-list-item-title>My profile</v-list-item-title>
             </v-list-item>
-            <v-list-item to="/administration" v-if="showAdmin">
-              <v-list-item-icon>
-                <v-icon>mdi-cogs</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Administration</v-list-item-title>
-            </v-list-item>
             <v-divider />
-            <v-list-item @click="$auth.logout({ returnTo })">
+            <v-list-item @click="signOut">
               <v-list-item-icon>
                 <v-icon>mdi-exit-run</v-icon>
               </v-list-item-icon>
@@ -85,39 +85,44 @@
           </v-list>
         </v-menu>
       </div>
+      <div v-else>
+        <router-link to="/sign-in">Sign in</router-link>
+      </div>
+
+      <!-- <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon> -->
     </v-app-bar>
 
     <v-main v-bind:style="{ 'padding-left: 33px !important': !hasSidebar }">
       <!-- Provides the application the proper gutter -->
-      <v-container fluid class="page-wrapper">
-        <router-view></router-view>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <router-view></router-view>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
-
-    <v-overlay v-model="showOverlay">
-      <div class="text-center">
-        <v-progress-circular
-          indeterminate
-          size="64"
-          class="mb-5"
-        ></v-progress-circular>
-        <h1 class="title">Loading Signing Authorities</h1>
-      </div>
-    </v-overlay>
   </v-app>
 </template>
 
 <script>
 import router from "@/router";
-// import store from "./store";
-
-import { mapActions } from "vuex";
+import store from "@/store";
 import * as config from "@/config";
-import { LOGOUT_URL } from "@/urls";
-//import { getInstance } from "@/auth/auth0-plugin";
+import { mapState } from "vuex";
 
 export default {
-  name: "Layout",
+  name: "App",
+  components: {},
+  computed: {
+    ...mapState("isAuthenticated"),
+    username() {
+      return store.getters.fullName;
+    },
+    isAuthenticated() {
+      return store.getters.isAuthenticated;
+    }
+  },
   data: () => ({
     dialog: false,
     drawer: null,
@@ -127,60 +132,39 @@ export default {
     loadingClass: "d-none",
     applicationName: config.applicationName,
     applicationIcon: config.applicationIcon,
-    sections: [],
+    sections: config.sections,
     hasSidebar: false, //config.hasSidebar,
-    hasSidebarClosable: config.hasSidebarClosable,
-    search: "",
-    showAdmin: true,
-    showOverlay: true,
+    hasSidebarClosable: config.hasSidebarClosable
   }),
-  computed: {
-    username() {
-      return this.$auth.user.name;
-    },
+  created: async function() {
+    await store.dispatch("checkAuthentication");
+    //this.username = store.getters.fullName
+    console.log(this.isAuthenticated);
 
-    returnTo: function () {
-      return config.applicationUrl;
-    },
-  },
-  async mounted() {
-    //let auth = await getInstance();
-    //await auth.getTokenSilently();
-    await this.initialize();
-    this.showOverlay = false;
+    if (!this.isAuthenticated) this.hasSidebar = false;
+    else this.hasSidebar = config.hasSidebar;
   },
   watch: {
-    $route(to) {
-      let meta = to.meta || {};
-
-      if (meta.children) {
-        this.sections = meta.children;
-        this.hasSidebar = true;
-      } else {
-        this.sections = [];
-        this.hasSidebar = false;
-      }
-    },
-    roles: function (val) {
-      this.showAdmin = false;
-      if (val.indexOf("Admin") >= 0) this.showAdmin = true;
-    },
+    isAuthenticated: function(val) {
+      if (!val) this.hasSidebar = false;
+      else this.hasSidebar = config.hasSidebar;
+    }
   },
   methods: {
-    ...mapActions(["initialize"]),
-    nav: function (location) {
+    nav: function(location) {
       router.push(location);
       console.log(location);
     },
-    toggleHeader: function () {
+    toggleHeader: function() {
       this.headerShow = !this.headerShow;
     },
-    toggleMenu: function () {
+    toggleMenu: function() {
       this.menuShow = !this.menuShow;
     },
-    signOut: function () {
-      window.location = LOGOUT_URL;
-    },
-  },
+    signOut: function() {
+      store.dispatch("signOut");
+      router.push("/");
+    }
+  }
 };
 </script>
--- src/web/src/modules/home/store/index.js
+++ src/web/src/modules/home/store/index.js
@@ -1,14 +1,24 @@
+import axios from "axios";
 
 const state = {
+    departments: [],
 };
 
 const actions = {
     async initialize() {
         console.log("-- Initializing Home Store")
     },
+    async loadDepartments({ commit }) {
+        axios.get("/departments").then(resp => {
+            commit("SET_DEPARTMENTS", resp.data)
+        })
+    },
 };
 
 const mutations = {
+    SET_DEPARTMENTS(state, value) {
+        state.departments = value;
+    }
 };
 
 export default {

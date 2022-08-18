d445121cec5635a0d4170d9e251993450f81f06f
Michael Johnson <michael@icefoganalytics.com>
2022-08-18 10:19:34 -0700

    Update config.js

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -10,16 +10,6 @@ export const sections = [
         url: "/",
         icon: "mdi-view-dashboard"
     },
-    {
-        name: "Basic Form",
-        url: "/form",
-        icon: "mdi-book-open-variant"
-    },
-    {
-        name: "Data grid",
-        url: "/grid",
-        icon: "mdi-table-large"
-    }
 ];
 export const environment = process.env.NODE_ENV;
 export const apiBaseUrl = process.env.NODE_ENV == "production" ? "" : "http://localhost:3000";

c8e0fdf875dbad5dec10d9739464d759ca8c96a0
Michael Johnson <michael@icefoganalytics.com>
2022-08-18 10:24:54 -0700

    Modules store

---
+++ src/web/src/modules/home/store/index.js
@@ -0,0 +1,19 @@
+
+const state = {
+};
+
+const actions = {
+    async initialize() {
+        console.log("-- Initializing Home Store")
+    },
+};
+
+const mutations = {
+};
+
+export default {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+};
--- src/web/src/store/index.js
+++ src/web/src/store/index.js
@@ -4,11 +4,13 @@ import Vuex from "vuex";
 import auth from "./auth";
 import profile from "./profile";
 
+import home from "@/modules/home/store"
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: { auth, profile }
+  modules: { auth, profile, home }
 });

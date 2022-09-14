531f81987c6bef450c6ac2d3a3e76887503bdfa5
Michael Johnson <michael@icefoganalytics.com>
2022-09-14 09:53:55 -0700

    Departments URL

--- src/web/src/modules/home/store/index.js
+++ src/web/src/modules/home/store/index.js
@@ -1,29 +1,30 @@
 import axios from "axios";
+import { DEPARTMENT_URL } from "@/urls";
 
 const state = {
-    departments: [],
+  departments: [],
 };
 
 const actions = {
-    async initialize() {
-        console.log("-- Initializing Home Store")
-    },
-    async loadDepartments({ commit }) {
-        axios.get("/departments").then(resp => {
-            commit("SET_DEPARTMENTS", resp.data)
-        })
-    },
+  async initialize() {
+    console.log("-- Initializing Home Store");
+  },
+  async loadDepartments({ commit }) {
+    axios.get(DEPARTMENT_URL).then((resp) => {
+      commit("SET_DEPARTMENTS", resp.data);
+    });
+  },
 };
 
 const mutations = {
-    SET_DEPARTMENTS(state, value) {
-        state.departments = value;
-    }
+  SET_DEPARTMENTS(state, value) {
+    state.departments = value;
+  },
 };
 
 export default {
-    namespaced: true,
-    state,
-    actions,
-    mutations
+  namespaced: true,
+  state,
+  actions,
+  mutations,
 };

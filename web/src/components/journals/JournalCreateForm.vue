5f1a975da202a27cb02a7ced0ec959f47dd4aad8
caleb <calebburke91@gmail.com>
2025-07-24 14:39:18 -0700

    better recoveries pick

--- web/src/components/journals/JournalCreateForm.vue
+++ web/src/components/journals/JournalCreateForm.vue
@@ -91,89 +91,24 @@
           :rules="[required]"
         />
       </v-col>
-      <v-col cols="4">
-        <v-btn
-          color="primary"
-          prepend-icon="mdi-refresh"
-          variant="outlined"
-          text="Refresh Recoveries"
-          @click="refreshRecoveriesList"
-        />
-      </v-col>
     </v-row>
 
     <h3 class="mb-3">Recoveries</h3>
 
-    <div v-if="totalRecoveries > 0">
-      <v-list>
-        <v-list-item>
-          <v-row>
-            <v-col cols="2"> Fiscal Year </v-col>
-            <v-col cols="2"> Supplier </v-col>
-            <v-col cols="4"> Branch </v-col>
-            <v-col cols="2"> Total Price </v-col>
-            <v-col cols="1"></v-col>
-          </v-row>
-        </v-list-item>
-        <div
-          v-for="(recovery, index) of recoveries"
-          :key="index"
-        >
-          <v-list-item>
-            <v-row>
-              <v-col cols="2">
-                <v-text-field
-                  v-model="recovery.fiscal_year"
-                  class="pt-2"
-                  density="compact"
-                  hide-details
-                  readonly
-                />
-              </v-col>
-              <v-col cols="2">
-                <v-text-field
-                  v-model="recovery.supplier"
-                  class="pt-2"
-                  density="compact"
-                  hide-details
-                  readonly
-                />
-              </v-col>
-              <v-col cols="4">
-                <v-text-field
-                  v-model="recovery.branch"
-                  class="pt-2"
-                  density="compact"
-                  hide-details
-                  readonly
-                />
-              </v-col>
-              <v-col cols="2">
-                <v-text-field
-                  :model-value="formatCurrency(recovery.totalPrice)"
-                  density="compact"
-                  hide-details
-                  readonly
-                />
-              </v-col>
-              <v-col cols="1">
-                <v-btn
-                  class="mt-1"
-                  icon="mdi-delete"
-                  size="x-small"
-                  color="error"
-                  @click="removeRecoveryAt(index)"
-                />
-              </v-col>
-            </v-row>
-          </v-list-item>
-        </div>
-      </v-list>
-    </div>
-    <div v-else>No recoveries found</div>
+    <p
+      v-if="isNil(journal.department)"
+      class="mb-3"
+    >
+      Please select a department before picking recoveries
+    </p>
+
+    <RecoveriesSearchableAutocomplete
+      v-model="recoveryIds"
+      :disabled="isNil(journal.department)"
+      :where="{ department: journal.department }"
+    />
 
     <v-divider class="my-5" />
-
     <v-btn
       type="submit"
       :loading="isCreating"
@@ -184,58 +119,29 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue"
+import { ref } from "vue"
 import { isNil } from "lodash"
 import { VForm } from "vuetify/components"
 
 import { required } from "@/utils/validators"
-import formatCurrency from "@/utils/format-currency"
 
 import journalApi, { Journal } from "@/api/journals-api"
 
 import useSnack from "@/use/use-snack"
-import useRecoveries, { type RecoveryWhereOptions } from "@/use/use-recoveries"
 
 import StringDateInput from "@/components/common/StringDateInput.vue"
 import FiscalYearSelect from "@/components/common/FiscalYearSelect.vue"
 import DepartmentSelectString from "@/components/departments/DepartmentSelectString.vue"
+import RecoveriesSearchableAutocomplete from "@/components/recoveries/RecoveriesSearchableAutocomplete.vue"
 
 const emit = defineEmits<{ created: [journalId: number] }>()
 
 const journal = ref<Partial<Journal>>({})
+const recoveryIds = ref<number[]>([])
 
 const isCreating = ref(false)
 const formRef = ref<InstanceType<typeof VForm> | null>(null)
 const snack = useSnack()
-
-const recoveriesWhereOptions = computed<RecoveryWhereOptions>(() => {
-  if (isNil(journal.value.department)) return {}
-
-  return {
-    department: journal.value.department,
-  }
-})
-
-const recoveriesQueryOptions = computed(() => ({
-  where: recoveriesWhereOptions.value,
-}))
-
-const {
-  recoveries,
-  totalCount: totalRecoveries,
-  refresh: refreshRecoveries,
-} = useRecoveries(recoveriesQueryOptions, {
-  skipWatchIf: () => isNil(journal.value.department),
-})
-
-function refreshRecoveriesList() {
-  if (isNil(journal.value.department)) return
-  refreshRecoveries()
-}
-
-function removeRecoveryAt(index: number) {
-  recoveries.value.splice(index, 1)
-}
 
 async function validateAndCreate() {
   if (formRef.value === null) return
@@ -245,10 +151,7 @@ async function validateAndCreate() {
 
   try {
     isCreating.value = true
-
-    const recoveryIds = recoveries.value.map((recovery) => recovery.recoveryID)
-    const data = await journalApi.create({ ...journal.value }, recoveryIds)
-
+    const data = await journalApi.create({ ...journal.value }, recoveryIds.value)
     emit("created", data.journal.journalID)
     close()
     snack.success("Journal created")
